c4ca248ebab3fe7daf5ddcd819afa8dcd14e5000
shchemer <gleb_2002@inbox.ru>
2023-05-06 15:00:33 +0300

    feat: complete task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const helloPhrase = 'Привет, введи текст';
+const goodByePhrase = 'Пока-пока';
+console.log(helloPhrase);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.on('line', data => {
+  if (data === 'exit') { process.exit() };
+  writeStream.write(`${data} \n`);
+});
+
+process.on('exit', () => {
+  console.log(goodByePhrase);
+})
