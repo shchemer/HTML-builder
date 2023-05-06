689b01392c128035864eb71335bcf4da6482e1af
shchemer <gleb_2002@inbox.ru>
2023-05-06 16:51:25 +0300

    feat: complete task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(filePath, {withFileTypes: true}, (error, list) => {
+  list.forEach(item => {
+    if (!item.isDirectory()) {
+      fs.stat(path.join(filePath, item.name), (error, stats) => {
+        const fileName = item.name.slice(0, item.name.lastIndexOf('.'));
+        const fileExt = path.extname(path.join(filePath, item.name)).replace('.', '');
+        const fileSize = stats.size / 1000;
+        console.log(`${fileName} - ${fileExt} - ${fileSize}kb`);
+      })
+    }
+  })
+});
