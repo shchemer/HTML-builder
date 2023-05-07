6039bfbdcd1de02fb195a9e935464a27df466245
shchemer <gleb_2002@inbox.ru>
2023-05-07 21:58:37 +0300

    fix: change logic for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,37 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const source = path.join(__dirname, 'files');
 const copy = path.join(__dirname, 'files-copy');
 
-fs.mkdir(copy, {recursive: true}, error => {
-  if (error) console.log(error);
-});
-
-fs.readdir(source, (error, files) => {
-  files.forEach(item => {
-    fs.copyFile(path.join(source, item), path.join(copy, item), error => {
-      if (error) console.log(error);
-    });
+fs.stat(copy)
+  .then(() => {
+    freshFiles();
+  })
+  .catch(() => {
+    fs.mkdir(copy);
+    freshFiles();
   });
-});
+
+async function freshFiles() {
+  await fs.readdir(copy)
+    .then(async files => {
+      if (files.length) {
+        for (let file of files) {
+          await fs.rm(path.join(copy, file), {force:true, recursive: true});
+        }
+      }
+    });
+  function addFiles(source, copy) {
+    fs.readdir(source, {withFileTypes:true}).then(files => {
+      files.forEach( file => {
+        if (file.isDirectory()) {
+          fs.mkdir(path.join(copy, file.name));
+          addFiles(path.join(source, file.name), path.join(copy, file.name));
+          return true;
+        }
+        fs.copyFile(path.join(source, file.name), path.join(copy, file.name));
+      });
+    });
+  }
+  addFiles(source, copy);
+}
